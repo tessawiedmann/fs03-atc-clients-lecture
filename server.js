163025874dfdcb38174c986ff97bb568d4a480aa
tessawiedmann <tlwiedmann@gmail.com>
2023-11-20 11:47:06 +0100

    added server.js file

---
+++ server.js
@@ -0,0 +1,12 @@
+const characters = require('./characters');
+const express = require('express');
+
+const app = express();
+const port = 3000;
+app.listen(port, () => {
+    console.log('the server has started!');
+});
+app.get('/characters', (request, response) => {
+    console.log(`received a request to ${request.path}`);
+    response.send(characters)
+});
